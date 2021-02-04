27ac23c6d344a8030d0993c03d4f1b15ae3264ae
Tony Fletcher <tfletch5@users.noreply.github.com>
2021-02-04 16:29:18 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+/**
+ * In the programming language of your choice, write a program that parses a sentence and replaces each
+ * word with the following: first letter, number of distinct characters between first and last character, and
+ * last letter.  For example, Smooth would become S3h.  Words are separated by spaces or non-alphabetic
+ * characters and these separators should be maintained in their original form and location in the answer.
+ */
+
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+
+  useEffect(() => {
+    let newSentence = "";
+    first.split(" ").forEach((f) => {
+      const count = String.prototype.concat(...new Set(f.slice(1, -1))).length;
+      newSentence += f.substr(0, 1) + count + f.substr(-1, 1) + " ";
+    });
+    setSecond(newSentence);
+  }, [first]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        id="sentence"
+        onChange={({ target }) => setFirst(target.value)}
+      />
+      <p>{first}</p>
+      <p>{second}</p>
+    </div>
+  );
+}
